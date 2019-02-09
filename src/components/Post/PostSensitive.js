d7d1a7cec6709519a46672e0e09160742a897311
Marquis Kurt <software@marquiskurt.net>
2019-02-09 16:59:16 -0500

    Change look of spoiler buttons based on pre-pending tags and add options for spoiler window

--- src/components/Post/PostSensitive.js
+++ src/components/Post/PostSensitive.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {CompoundButton, Dialog, DialogType} from "office-ui-fabric-react";
+import {ColorClassNames} from '@uifabric/styling';
 
 class PostSensitive extends Component {
     constructor(props) {
@@ -19,12 +20,46 @@ class PostSensitive extends Component {
 
     }
 
+    getAdditionalInformation(spoiler) {
+        if (spoiler.includes('NSFW: ')) {
+            return 'Careful! This content isn\'t safe for work.';
+        } else if (spoiler.includes('Spoiler: ')) {
+            return 'If you haven\'t experienced the topic beforehand, you shouldn\'t view this.';
+        } else {
+            return 'Proceed with caution.';
+        }
+    }
+
+    flagColorOfButton(spoiler) {
+        if (spoiler.includes('NSFW: ')) {
+            return [ColorClassNames.redDarkBackground, ColorClassNames.redDarkBackgroundHover, ColorClassNames.white, ColorClassNames.whiteHover];
+        } else if (spoiler.includes('Spoiler: ')) {
+            return [ColorClassNames.yellowBackground, ColorClassNames.yellowBackgroundHover];
+        } else {
+            return [];
+        }
+    }
+
+    primaryOrNot(spoiler) {
+        if (spoiler.includes('NSFW: ')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     render() {
         let status = this.state.status;
         return (
             <div className="mt-2">
-                <CompoundButton primary={false} secondaryText={status.spoiler_text} onClick={this.toggle}>
-                    Show spoiler
+                <CompoundButton
+                    primary={this.primaryOrNot(status.spoiler_text)}
+                    secondaryText={this.getAdditionalInformation(status.spoiler_text)}
+                    onClick={this.toggle}
+                    text={status.spoiler_text || "Content warning"}
+                    className={this.flagColorOfButton(status.spoiler_text)}
+                >
+
                 </CompoundButton>
                 <Dialog
                     hidden={!this.state.modal}
