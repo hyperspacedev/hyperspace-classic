071785583b7d2950391175e9c33495d470fde47d
Marquis Kurt <software@marquiskurt.net>
2019-03-02 14:28:49 -0500

    Move auth section to login window instead

--- src/components/Post/PostContent.tsx
+++ src/components/Post/PostContent.tsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class PostContent extends Component {
     render() {
         return (
-            <div className="post-content">{this.props.children}</div>
+            <div className="post-content text-break">{this.props.children}</div>
         );
     }
 }
