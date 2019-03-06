f4fb67febaf332b7966447b1be3a7cc6b53436e6
Marquis Kurt <software@marquiskurt.net>
2019-03-06 15:07:55 -0500

    Add basic toasts

---
+++ src/components/Toast/index.tsx
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {getDarkMode} from '../../utilities/getDarkMode';
+
+export class Toast extends Component<any, any> {
+
+    getStyles() {
+        if (getDarkMode() == "dark") {
+            return {
+                marginTop: 64,
+                paddingTop: 16,
+                paddingLeft: 24,
+                paddingBottom: 4,
+                backgroundColor: "#333",
+                color: "#f4f4f4"
+            }
+        } else {
+            return {
+                marginTop: 64,
+                paddingTop: 16,
+                paddingLeft: 24,
+                paddingBottom: 4,
+                backgroundColor: "#fff",
+                color: "#f4f4f4"
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div className = {"shadow rounded marked-area" + getDarkMode()} style = {this.getStyles()}>
+                <p><b>{this.props.text}</b></p>
+            </div>
+        );
+    }
+}
